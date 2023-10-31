5b8710df7295ca820948c38a90edf0d9580975dd
Helen <helendianaf@gmail.com>
2023-10-31 10:53:01 -0300

    add exercicios beecrowd

---
+++ src/beecrowd/Exercicio1000.java
@@ -0,0 +1,14 @@
+/*
+O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!".
+Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+Saída
+Você deve imprimir a mensagem "Hello World!"
+*/
+
+package beecrowd;
+
+public class Exercicio1000 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/beecrowd/Exercicio1001.java
@@ -0,0 +1,20 @@
+/*
+Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de A e B atribuindo
+o seu resultado na variável X. Imprima X . Não apresente
+mensagem alguma além daquilo que está sendo especificado.
+
+Saída:
+Imprima a mensagem "X = " (letra X maiúscula) seguido pelo valor da variável X e pelo final
+de linha. Cuide para que tenha um espaço antes e depois do sinal de igualdade, conforme o exemplo abaixo.
+*/
+package beecrowd;
+
+public class Exercicio1001 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 9;
+        int x = a + b;
+
+        System.out.println("X = " + x);
+    }
+}
