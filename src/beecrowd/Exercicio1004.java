b56af39a93f054b1b58296ed226e24647e610536
Helen <helendianaf@gmail.com>
2023-11-01 15:06:02 -0300

    update exercise list beecrowd

---
+++ src/beecrowd/Exercicio1004.java
@@ -0,0 +1,24 @@
+/*
+Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores
+e atribua esta operação à variável PROD. A seguir mostre a variável PROD com mensagem
+correspondente.
+
+Entrada
+O arquivo de entrada contém 2 valores inteiros.
+
+Saída
+Imprima a mensagem "PROD" e a variável PROD conforme exemplo abaixo, com um espaço em
+branco antes e depois da igualdade.
+*/
+
+package beecrowd;
+
+public class Exercicio1004 {
+    public static void main(String[] args) {
+        int valorUm = 1004;
+        int valorDois = 96;
+        int PROD = valorUm * valorDois;
+
+        System.out.println("PROD = " + PROD);
+    }
+}
